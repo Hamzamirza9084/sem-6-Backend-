36444231fd63729385ab319f0e63f34ba19a1498
Hamzamirza9084 <132834994+Hamzamirza9084@users.noreply.github.com>
2025-03-15 23:19:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://sage-empanada-544ff4.netlify.app'], 
+    origin: ['http://localhost:5173'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true, 
 };
