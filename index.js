265b35d4853c5df9a5723aeae8e6b2425b201200
Hamzamirza9084 <132834994+Hamzamirza9084@users.noreply.github.com>
2025-03-15 19:44:26 +0530

    Update index.js
    update

--- index.js
+++ index.js
@@ -9,16 +9,28 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    origin:["http://localhost:5173"],
-    credentials: true
-}))
+
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://gregarious-fudge-8d4f5d.netlify.app'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true, 
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser())
 app.use('/auth',UserRouter)
 
-mongoose.connect(process.env.MURL)
+mongoose
+    .connect(process.env.MURL{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 
-
+app.get("/", (req, res) => {
+    res.json("Hello");
+});
 
 app.listen(process.env.PORT,()=>{
     console.log("server is running")
