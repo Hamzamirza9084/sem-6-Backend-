6300f41d4432d30c4472fbe449fc1188bf34d8c3
Hamzamirza9084 <hamzamirza9084@gmail.com>
2025-04-22 00:57:41 +0530

    deploy

--- index.js
+++ index.js
@@ -8,28 +8,31 @@ import cookieParser from 'cookie-parser'
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
 
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'yoursecretkey',
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.MURL }), // Optional: store in MongoDB
-  cookie: {
-    maxAge: 3600000, // 1 hour
-    secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
-    httpOnly: true
-  }
-}));
+
 
 
 const app = express()
 
 app.use(express.json())
 
+
 const corsOptions = {
     origin: ['http://localhost:5173','https://frolicking-marigold-49ce1d.netlify.app'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     credentials: true, 
 };
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'yoursecretkey',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MURL }), // Optional: store in MongoDB
+    cookie: {
+      maxAge: 3600000, // 1 hour
+      secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+      httpOnly: true
+    }
+  }));
 
 app.use(cors(corsOptions));
 app.use(cookieParser())
